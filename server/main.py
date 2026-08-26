3ba5d2262b7aabcc75f3d7c83cb863dfcc517a32
Oleksandr <allekksszalevskyi@gmail.com>
2026-08-27 01:12:57 +0300

    fix: allow all origins in CORS

--- server/main.py
+++ server/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="Applicant Test API")
 # Setup CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_url, "http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
