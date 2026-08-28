df2ec91066019dc0de346971f61de221090d6df1
Oleksandr <allekksszalevskyi@gmail.com>
2026-08-29 00:23:05 +0300

    fix: protect /test and /results routes from unauthenticated access

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/login";
 import Test from "./components/test";
 import Results from "./components/results";
+
+// A wrapper for protected routes
+function ProtectedRoute({ children }) {
+  const applicantId = localStorage.getItem('applicantId');
+  if (!applicantId) {
+    // If not registered/logged in, redirect to home page
+    return <Navigate to="/" replace />;
+  }
+  return children;
+}
 
 export default function App() {
   return (
@@ -9,8 +19,22 @@ export default function App() {
       <div className="app-container">
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/test" element={<Test />} />
-          <Route path="/results" element={<Results />} />
+          <Route 
+            path="/test" 
+            element={
+              <ProtectedRoute>
+                <Test />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/results" 
+            element={
+              <ProtectedRoute>
+                <Results />
+              </ProtectedRoute>
+            } 
+          />
         </Routes>
       </div>
     </BrowserRouter>
